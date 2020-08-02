a13671d49045957f1d95d3f1289d5fb9fefba1e5
Birte Blankenhaus <BibiBla@users.noreply.github.com>
2020-08-02 13:05:40 +0100

    added weather API
    City and temperature fetched from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Hello!");
+function displayTemperature(response) {
+  console.log(response);
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+}
+
+let apiKey = "4190a6ee70227a6b15b76f600409fe74";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
