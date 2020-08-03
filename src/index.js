a2b1ac495881f95680a0c88cf2f6b429919d67f7
Birte Blankenhaus <BibiBla@users.noreply.github.com>
2020-08-03 18:33:00 +0100

    Linked cityInput.value with axios
    Now the search is working and updating the weather data for the search input

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-function search(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
-  console.log(cityInput);
-  let city = document.querySelector("#city");
-  city.innerHTML = cityInput.value;
-}
-
 function formatTime(timestamp) {
   let date = new Date(timestamp);
   console.log(date);
@@ -60,10 +52,17 @@ function displayTemperature(response) {
   time.innerHTML = formatTime(response.data.dt * 1000);
 }
 
-let apiKey = "4190a6ee70227a6b15b76f600409fe74";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "4190a6ee70227a6b15b76f600409fe74";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+}
 
 let citySearch = document.querySelector("#search-form");
-citySearch.addEventListener("submit", search);
+citySearch.addEventListener("submit", handleSubmit);
