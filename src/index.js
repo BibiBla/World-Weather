3ff066eb1c0ac0fe7366e71d15eb0749fbdf1267
Birte Blankenhaus <BibiBla@users.noreply.github.com>
2020-08-03 18:22:47 +0100

    Linked searchengine to JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  console.log(cityInput);
+  let city = document.querySelector("#city");
+  city.innerHTML = cityInput.value;
+}
+
 function formatTime(timestamp) {
   let date = new Date(timestamp);
   console.log(date);
@@ -56,3 +64,6 @@ let apiKey = "4190a6ee70227a6b15b76f600409fe74";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+let citySearch = document.querySelector("#search-form");
+citySearch.addEventListener("submit", search);
